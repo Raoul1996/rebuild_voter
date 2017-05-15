9ca480ea8473f2f26f3748ea283700b073d047be
周宝 <baobaozhou199606@gmail.com>
2017-05-15 20:49:45 +0800

    更新list age内容的样式
    Change-Id: I41b122361878f078c98ec282371374ca46bb779c

--- app/src/components/list/showList/index.js
+++ app/src/components/list/showList/index.js
@@ -1,17 +1,40 @@
-import React,{Component} from 'react'
-import { Card, Col, Row } from 'antd';
+import React, { Component } from 'react'
+import { Card, Col, Row, Icon } from 'antd'
 import './index.less'
+import avatar from './avatar.png'
+// 投票的标题
+let title = '不洗碗工作室最美程序员选举选举'
+// 投票的人数
+let voterNum = 10
+let runing = true
+function limitStringNum (title) {
+  if (title.length > 10) {
+    return `${title.substr(0, 8)}……`
+  } else {
+    return title
+  }
+}
+
 class ShowList extends Component {
-  render() {
-    return(
-      <div>
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    // let title = this.props.title
+    return (
+      <div className="list-item-wrapper">
         <Card>
-          <div className="custom-image">
-            <img alt="example" width="100%" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+          <div className="item-title">
+            <h3>{limitStringNum(title)}</h3>
           </div>
-          <div className="custom-card">
-            <h3>Europe Street beat</h3>
-            <p>www.instagram.com</p>
+          <div className="item-voter">
+            <img src={avatar} className="item-voter-icon" />
+            <span className="item-voter-num">{voterNum}</span>
+          </div>
+          <div className="item-status">
+            <div className="item-status-text item-status-item">{runing ? '进行中' : '未开始'}</div>
+            <div className="item-status-time item-status-item">剩余{'5小时25分钟'}</div>
           </div>
         </Card>
       </div>
--- app/src/pages/index/list/index.js
+++ app/src/pages/index/list/index.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react'
-import {Row,Col} from 'antd'
+import { Row, Col } from 'antd'
 import List from '../../../components/list/showList'
 import LineText from '../../../components/content/lineText'
 import MsgListComponent from '../../../components/content/msgList'
-import ShowList from '../../../components/list/showList/index'
 import './index.less'
-class Item extends Component{
+class Item extends Component {
   constructor (props) {
     super(props)
   }
-  render() {
-    return(
+
+  render () {
+    return (
       <div className="show-list-wrapper">
         <div className="msg-bar">
-          <MsgListComponent label="手机号" text="15012321234"/>
+          <MsgListComponent label="手机号" text="15012321234" />
         </div>
         <div className="show-list">
+          <Row>
+            <Col span="10" offset={1}>
+              <List />
+            </Col>
+            <Col span="10" offset={2}>
+              <List />
+            </Col>
+          </Row>
+          <Row>
+            <Col span="10" offset={1}>
+              <List />
+            </Col>
+            <Col span="10" offset={2}>
+              <List />
+            </Col>
+          </Row>
+          <Row>
+            <Col span="10" offset={1}>
+              <List />
+            </Col>
+            <Col span="10" offset={2}>
+              <List />
+            </Col>
+          </Row>
           <Row>
             <Col span="10" offset={1}>
               <List />
