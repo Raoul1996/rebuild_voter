c3a7cc8150487047c16a88cc024cc91d4f114357
周宝 <zhoubao@hrsoft.net>
2017-06-06 22:21:46 +0800

    【story = 165】 引用babel-polyfill
    Change-Id: I06773d7a1d386a9d5b1aba148da2eb3f586c2754

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import RouterApp from './router/router.js'
-
+import 'babel-polyfill'
 ReactDOM.render(<RouterApp/>, document.getElementById('app'))
